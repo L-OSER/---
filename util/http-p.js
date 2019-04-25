e3cb49a413d18bb72a48ae4d03c3680c170f3d06
zhoujiajun <770236524@qq.com>
2019-04-25 18:12:18 +0800

    完成书籍界面

--- util/http-p.js
+++ util/http-p.js
@@ -7,30 +7,37 @@ const tips = {
 }
 
 class HTTP{
-    request(params){
-        if (!params.method){
-            params.method = 'GET'
-        }
+    // #解构
+    request({url,data={},method='GET'}){
+        return new Promise((resolve,reject)=>{
+            this._request(url,resolve,reject,data,method)
+        })
+    }
+
+    _request(url,resolve,reject,data={},method='GET'){
+
         wx.request({
-            url:config.api_base_url + params.url,
-            method:params.method,
-            data:params.data,
+            url:config.api_base_url + url,
+            method:method,
+            data:data,
             header:{
                 'content-type':'application/json',
                 'appkey':config.appkey
             },
             success:(res)=>{
-                let code = res.statusCode.toString()
+                const code = res.statusCode.toString()
                 // 判断状态码开头是否是2,
                 if (code.startsWith('2')){
                     // 判断是否有回调函数,有的话调用后面的函数 &&
-                    params.success && params.success(res.data)
+                    resolve(res.data)
                 } else {
+                    reject()
                     let error_code = res.data.error_code
                     this._show_error(error_code)
                 }
             },
             fail:(err)=>{
+                reject()
                 this._show_error(1)
             }
         })
